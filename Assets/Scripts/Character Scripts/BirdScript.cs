a3c84845bd15a2cc90b40bbf3a0caeb5cf1b32a4
Sky <yoayo112@gmail.com>
2024-02-06 23:21:04 -0700

    more merge conflicts?

---
+++ Assets/Scripts/Character Scripts/BirdScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Initialize
+
+        //generate wait time (random?)
+        
+        // Wait/set idle trigger
+        
+        //check for wait time trigger
+
+        //get new location
+
+        //trigger flap
+
+        //move to new location
+
+        //set wait/set idle trigger.
+        
+    }
+}
