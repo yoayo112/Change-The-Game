342b9baece7a8634b702fad08df7804f57d9337b
Elijah Theander <elijahtheander@gmail.com>
2024-02-26 20:28:17 -0600

    Testing UnityEvents

---
+++ Assets/damageTaker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class damageTaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void onDamage()
+    {
+        Debug.Log("On damage called");
+    }
+}
