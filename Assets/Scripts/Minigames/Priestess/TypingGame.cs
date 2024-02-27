732a28865fa95c2028c2191c972309cb6f7e6929
Sean <seanthornto@gmail.com>
2024-02-27 13:51:45 -0700

    Added comments to TypingGame.cs

--- Assets/Scripts/Minigames/Priestess/TypingGame.cs
+++ Assets/Scripts/Minigames/Priestess/TypingGame.cs
@@ -37,9 +37,12 @@ public class TypingGame : MonoBehaviour
         Update_Mistake_Display();
     }
 
+    //-------------------------------------------------------------------------------------
+    //  Mutators
+    //-------------------------------------------------------------------------------------
     private void Set_Current_Word()
+        //Sets _remainingWord and resets _typedWord and _currentLetter
     {
-        // Get word from bank
         _isWordComplete = false;
         _typedWord = string.Empty;
         _currentLetter = _currentWord[0];
@@ -47,12 +50,57 @@ public class TypingGame : MonoBehaviour
     }
 
     private void Set_Remaining_Word(string newString_)
+        //Sets _remainingWord to given string
     {
         _remainingWord = newString_;
         Display_Word();
     }
 
+    private void Display_Word()
+        //Updates wordOutput with _typedWord, _currentLetter, and _remainingWord
+    {
+        char dispCurrentLetter_;
+        if (char.IsWhiteSpace(_currentLetter))
+            dispCurrentLetter_ = '█';
+        else
+            dispCurrentLetter_ = _currentLetter;
+
+        wordOutput.text = "<color=green>" + _typedWord + "</color><color=yellow>" + dispCurrentLetter_ + "</color>" + _remainingWord;
+    }
+
+    private void Remove_Letter()
+        //Adds typed letter to _typedWord, gets new _currentLetter from _remainingWord, and removes letter from _remainingWord
+    {
+        if (_remainingWord.Length == 0)
+            _isWordComplete = true;
+        else
+        {
+            _typedWord += _currentLetter;
+            _currentLetter = _remainingWord[0];
+            string newString_ = _remainingWord.Remove(0, 1);
+            Set_Remaining_Word(newString_);
+        }
+    }
+
+    private void Add_Mistake()
+        //Increments mistake counter, plays sound, and locks player input
+    {
+        _mistakes++;
+        //Camera Shake
+        Update_Mistake_Display();
+        audioSource.PlayOneShot(audioClipArray[0], volume);
+        StartCoroutine(Lock_Input());
+
+    }
+
+    private void Update_Mistake_Display()
+        //Updates mistake counter
+    {
+        mistakesOutput.text = "Mistakes: " + _mistakes;
+    }
+
     private void Check_Input()
+        //Pulls the player input if it is a single key press and calls Enter_Letter
     {
         if(Input.anyKeyDown)
         {
@@ -64,6 +112,7 @@ public class TypingGame : MonoBehaviour
     }
 
     private void Enter_Letter(string typedLetter_)
+        //When letter is typed, addes letter to typed word if correct, or adds mistake otherwise.
     {
         if (!_isLocked)
         {
@@ -79,62 +128,23 @@ public class TypingGame : MonoBehaviour
         }
     }
 
+    IEnumerator Lock_Input()
+        //Locks out player input for lockoutTime seconds
+    {
+        _isLocked = true;
+        yield return new WaitForSeconds(lockoutTime);
+        _isLocked = false;
+    }
+
+    //-------------------------------------------------------------------------------------
+    //  Accessors
+    //-------------------------------------------------------------------------------------
+
     private bool Is_Correct_Letter(string letter_)
     {
         return _currentLetter == char.ToLower(letter_[0]);
     }
 
-    private void Display_Word()
-    {
-        char dispCurrentLetter_;
-        if (char.IsWhiteSpace(_currentLetter))
-            dispCurrentLetter_ = '█';
-        else
-            dispCurrentLetter_ = _currentLetter;
-
-        wordOutput.text = "<color=green>" + _typedWord + "</color><color=yellow>" + dispCurrentLetter_ + "</color>" + _remainingWord;
-    }
-
-    private void Remove_Letter()
-    {
-        if (_remainingWord.Length == 0)
-            _isWordComplete = true;
-        else
-        {
-            _typedWord += _currentLetter;
-            Next_Letter();
-            string newString = _remainingWord.Remove(0, 1);
-            Set_Remaining_Word(newString);
-        }
-    }
-
-    private void Next_Letter()
-    {
-            _currentLetter = _remainingWord[0];
-    }
-
-    private void Add_Mistake()
-    {
-        _mistakes++;
-        //Camera Shake
-        Update_Mistake_Display();
-        audioSource.PlayOneShot(audioClipArray[0], volume);
-        StartCoroutine(Lock_Input());
-
-    }
-
-    private void Update_Mistake_Display()
-    {
-        mistakesOutput.text = "Mistakes: " + _mistakes;
-    }
-
-    IEnumerator Lock_Input()
-    {
-
-        _isLocked = true;
-        yield return new WaitForSeconds(lockoutTime);
-        _isLocked = false;
-
-    }
+ 
 }
 
