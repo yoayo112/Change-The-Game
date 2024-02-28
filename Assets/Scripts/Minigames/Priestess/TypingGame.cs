82c5d3d68ffad0b4c179e23094e9326492649577
Sean <seanthornto@gmail.com>
2024-02-28 15:48:35 -0700

    Typing Game: added available lines on the right to type from

--- Assets/Scripts/Minigames/Priestess/TypingGame.cs
+++ Assets/Scripts/Minigames/Priestess/TypingGame.cs
@@ -6,9 +6,18 @@ using TMPro;
 
 public class TypingGame : MonoBehaviour
 {
-    // Line bank needed
-    public TextMeshProUGUI LineOutput = null;
+    //-------------------------------------------------------------------------------------
+    //  Output
+    //-------------------------------------------------------------------------------------
+    [Header("Output")]
+    public TextMeshProUGUI typedLineOutput = null;
+    public TextMeshProUGUI availableLineOutput = null;
     public Text mistakesOutput = null;
+
+    //-------------------------------------------------------------------------------------
+    //  Settings
+    //-------------------------------------------------------------------------------------
+    [Header("Game Settings")]
     public float lockoutTime = 1f;
 
     //-------------------------------------------------------------------------------------
@@ -19,14 +28,18 @@ public class TypingGame : MonoBehaviour
     public AudioClip[] audioClipArray;
     public float volume = 0.5f;
 
+    //-------------------------------------------------------------------------------------
+    //  Variables
+    //-------------------------------------------------------------------------------------
     private string _typedLine = string.Empty;
-    private char _currentLetter;
-    private string _remainingLine = string.Empty;
-    private string _currentLine = "should be 28 chars or less";
     private string _previousLines = "";
-    private bool _isLineComplete = false;
-    private int _mistakes = 0;
+    private int _typedCount = 0;
+
+    private int _mistakeCount = 0;
     private bool _isLocked = false;
+
+    private string[] _availableLines = { "you can type this line.", "you can also type this.", "you even can type this." };
+    private bool[] _isActiveAvailableLines = { true, true, true };
 
     private void Update()
     {
@@ -35,50 +48,49 @@ public class TypingGame : MonoBehaviour
 
     private void Start()
     {
-        Set_Current_Line();
+        Display_Available_Lines();
+        Display_Typed_Line();
         Update_Mistake_Display();
     }
 
     //-------------------------------------------------------------------------------------
     //  Mutators
     //-------------------------------------------------------------------------------------
-    private void Set_Current_Line()
-        //Sets _remainingLine and resets _typedLine and _currentLetter
-    {
-        _isLineComplete = false;
-        _typedLine = string.Empty;
-        _currentLetter = _currentLine[0];
-        Set_Remaining_Line(_currentLine.Remove(0, 1));
-    }
+ 
 
-    private void Set_Remaining_Line(string newString_)
-        //Sets _remainingLine to given string
+    private void Display_Typed_Line()
+    //Updates typedLineOutput with _typedLine
     {
-        _remainingLine = newString_;
-        Display_Line();
-    }
-
-    private void Display_Line()
-        //Updates LineOutput with _typedLine, _currentLetter, and _remainingLine
-    {
-        string dispCurrentLetter_ = string.Empty;
-        string dispRemainingLine_ = _remainingLine;
         string dispTypedLine_ = _typedLine;
 
-        if (char.IsWhiteSpace(_currentLetter))
-            dispCurrentLetter_ = "_\u200B";
-        else
-            dispCurrentLetter_ = char.ToString(_currentLetter);
-
-        LineOutput.text = _previousLines + "<color=green>" + dispTypedLine_ + "</color><color=yellow>" + dispCurrentLetter_ + "</color>" + dispRemainingLine_;
+        typedLineOutput.text = dispTypedLine_;
     }
 
-    
+    private void Display_Available_Lines()
+        //Updates availableLineOutput with available lines, and progress on them.
+    {
+        string dispCurrentLetter_ = string.Empty;
+        string dispRemainingLine_ = string.Empty;
+        string dispTypedLine_ = _typedLine;
+
+        availableLineOutput.text = string.Empty;
+        for (int i = 0; i < _availableLines.Length; i++)
+        {
+            if (_isActiveAvailableLines[i])
+            {
+                dispCurrentLetter_ = Space_To_Underscore(_availableLines[i][_typedCount]);
+                dispRemainingLine_ = _availableLines[i].Remove(0, _typedCount + 1);
+                availableLineOutput.text += "<color=green>" + dispTypedLine_ + "</color><color=yellow>" + dispCurrentLetter_ + "</color>" + dispRemainingLine_ + "\n";
+            }
+            else
+                availableLineOutput.text += "<color=grey>" + _availableLines[i] + "</color>\n";
+        }
+    }
 
     private void Add_Mistake()
         //Increments mistake counter, plays sound, and locks player input
     {
-        _mistakes++;
+        _mistakeCount++;
         //Camera Shake
         Update_Mistake_Display();
         audioSource.PlayOneShot(audioClipArray[0], volume);
@@ -88,7 +100,7 @@ public class TypingGame : MonoBehaviour
     private void Update_Mistake_Display()
         //Updates mistake counter
     {
-        mistakesOutput.text = "Mistakes: " + _mistakes;
+        mistakesOutput.text = "Mistakes: " + _mistakeCount;
     }
 
     private void Check_Input()
@@ -99,38 +111,53 @@ public class TypingGame : MonoBehaviour
             string keysPressed_ = Input.inputString;
 
             if (keysPressed_.Length == 1)
-                Enter_Letter(keysPressed_);
+                Attempt_Letter(keysPressed_);
         }
     }
 
-    private void Enter_Letter(string typedLetter_)
+    private void Attempt_Letter(string typedLetter_)
         //When letter is typed, addes letter to typed Line if correct, or adds mistake otherwise. Will not add mistake if on lockout
     {
-        if (Is_Correct_Letter(typedLetter_))
-            Next_Letter();
+        bool isCorrect_ = false;
+        for (int i = 0; i < _availableLines.Length; i++)
+        {
+            if (Is_Correct_Letter(typedLetter_, _availableLines[i]) && _isActiveAvailableLines[i])
+                isCorrect_ = true;
+        }
 
+        if (isCorrect_)
+        {
+            for (int i = 0; i < _availableLines.Length; i++)
+            {
+                if (!Is_Correct_Letter(typedLetter_, _availableLines[i]))
+                    _isActiveAvailableLines[i] = false;
+            }
+            Enter_Letter(typedLetter_);
+        }
         else if (!_isLocked)
             Add_Mistake();
     }
 
-    private void Next_Letter()
+    private void Enter_Letter(string typedLetter_)
     //Adds typed letter to _typedLine, gets new _currentLetter from _remainingLine, and removes letter from _remainingLine
     {
-        if (_remainingLine.Length == 0)
-            Next_Line();
-        else
+        _typedCount++;
+        if (_typedCount == _availableLines[0].Length) //Reset for testing
         {
-            _typedLine += _currentLetter;
-            _currentLetter = _remainingLine[0];
-            string newString_ = _remainingLine.Remove(0, 1);
-            Set_Remaining_Line(newString_);
-        }
-    }
+            _typedCount = 0;
+            _typedLine = string.Empty;
+            for (int i = 0; i < _isActiveAvailableLines.Length; i++)
+                _isActiveAvailableLines[i] = true;
 
-    private void Next_Line()
-    {
-        _previousLines += "<color=green>" + _currentLine + "</color>\n";
-        Set_Current_Line();
+        }
+        else
+            _typedLine += typedLetter_;
+
+        Display_Typed_Line();
+        Display_Available_Lines();
+
+
+
     }
 
     IEnumerator Lock_Mistake()
@@ -145,11 +172,23 @@ public class TypingGame : MonoBehaviour
     //  Accessors
     //-------------------------------------------------------------------------------------
 
-    private bool Is_Correct_Letter(string letter_)
+    private bool Is_Correct_Letter(string letter_, string line_)
     {
-        return _currentLetter == char.ToLower(letter_[0]);
+        return char.ToLower(letter_[0]) == line_[_typedCount];
     }
 
- 
+    //-------------------------------------------------------------------------------------
+    //  Functions
+    //-------------------------------------------------------------------------------------
+
+    private string Space_To_Underscore(char character)
+    {
+        if (char.IsWhiteSpace(character))
+            return "_";
+        else
+            return char.ToString(character);
+    }
+
+
 }
 
