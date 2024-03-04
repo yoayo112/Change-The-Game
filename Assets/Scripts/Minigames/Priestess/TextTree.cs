66a0622c558dd911f6c890211bfeaaa6a8a6bcb5
Sean <seanthornto@gmail.com>
2024-03-03 20:10:30 -0700

    Recreated branch.

--- Assets/Scripts/Minigames/Priestess/TextTree.cs
+++ Assets/Scripts/Minigames/Priestess/TextTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
 
 // The TextTree object contains other TextTrees as branches. Each tree in the structure carries a string representing the shared text of all of it's branches.
 // Branching represents differences between the strings in the array that cosntructor uses as input.
@@ -19,17 +20,17 @@ public class TextTree
     public string text;
     public List<TextTree> branches;
 
-    public TextTree(string string_)
-    {
-        root = null;
-        text = string_;
-        branches = new List<TextTree>();
-    }
+    public TextTree Get_Root() => root;
+    public string Get_Text() => text;
+    public List<TextTree> Get_Branches() => branches;
+
+    public void Set_Root(TextTree root_) => root = root_;
+    public void Set_Text(string text_) => text = text_;
 
     public TextTree(string[] strings_)
     {
-        root = null;
-        text = Find_Common_String(strings_);    //The text for this branch of the tree is the common starting string among all input strings. The root text can be empty if there is no common string.
+        Set_Root(null);
+        Set_Text(Find_Common_String(strings_));  //The text for this branch of the tree is the common starting string among all input strings. The root text can be empty if there is no common string.
         branches = new List<TextTree>();
         for (int i = 0; i < strings_.Length; i++) //To build out the branches, we remove this common string from each input string
             strings_[i] = strings_[i].Remove(0, text.Length);
@@ -37,10 +38,35 @@ public class TextTree
         Build_Branches(strings_);
     }
 
+    public TextTree(string file_) : this(new string[0])
+    {
+        string line_;
+        List<string> lines_ = new List<string>();
+
+        try
+        {
+            StreamReader reader_ = new StreamReader(file_);
+            line_ = reader_.ReadLine();
+            while (line_ != null)
+            {
+                lines_.Add(line_);
+                line = reader_.ReadLine();
+            }
+            reader_.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Exception: " + e.Message);
+        }
+
+        Build_Branches(lines_.ToArray());
+    }
+
     public void Build_Branches(string[] strings_)
     // Adds branches to this tree based on given array of strings
     {
-        if (strings_[0].Length == 0) return; //No more branches to build if input string is empty. 
+        if (strings_.Length == 0 || strings_[0].Length == 0)
+            return; //No more branches to build if input string or array is empty.
 
         bool[] grouped_ = new bool[strings_.Length]; //grouped_[i] == true if the string strings_[i] has already been grouped
         List<string> group_ = new List<string>();
@@ -53,7 +79,7 @@ public class TextTree
             group_.Add(strings_[i]);
             for (int j = 1; i + j < strings_.Length; j++) //We compare the string only to strings further down the list as earlier strings have already been grouped
             {
-                if ((Has_Common_String(strings_[i], strings_[i + j]) && !grouped_[i + j]))
+                if (!grouped_[i + j] && Has_Common_String(strings_[i], strings_[i + j]))
                 {                                                                         //If the second string has not been grouped AND the strings share a starting string:
                     grouped_[i + j] = true;                                               //Mark new string as grouped
                     group_.Add(strings_[i + j]);                                          //Add the second string to the group
@@ -63,14 +89,34 @@ public class TextTree
         }
     }
 
-    public TextTree Get_Root()
+    public void Add_Branch(TextTree branch_)
     {
-        return root;
+        branch_.Set_Root(this);
+        branches.Add(branch_);
     }
 
-    public string Get_Text()
+    public void Remove_Branch(TextTree branch_)
     {
-        return text;
+        branch_.Set_Root(null);
+        branches.Remove(branch_);
+    }
+
+    public void Remove_Branch(string branchText_)
+    {
+        foreach (TextTree branch in branches)
+        {
+            if (branch.Get_Text() == branchText_)
+            {
+                Remove_Branch(branch);
+                break;
+            }
+        }
+    }
+
+    public void Remove_All_Branches()
+    {
+        foreach (TextTree branch in branches)
+            Remove_Branch(branch);
     }
 
     public string Get_Text_Upto_Branch()
@@ -79,27 +125,6 @@ public class TextTree
         if (root == null)
             return string.Empty;
         return root.Get_Text_Upto_Branch() + text;
-    }
-
-    public List<TextTree> Get_Branches()
-    {
-        return branches;
-    }
-
-    public void Set_Root(TextTree root_)
-    {
-        root = root_;
-    }
-
-    public void Set_Text(string text_)
-    {
-        text = text_;
-    }
-
-    public void Add_Branch(TextTree branch_)
-    {
-        branch_.Set_Root(this);
-        branches.Add(branch_);
     }
 
     private string Find_Common_String(string stringA_, string stringB_)
@@ -138,18 +163,18 @@ public class TextTree
         return stringA_[0] == stringB_[0];
     }
 
-    //private List<string> Find_Common_Strings(string[] strings_, string string_)
-    //{
-    //    List<string> output = new List<string>();
-    //    string commonString_;
-    //
-    //    for (int i = 0; i < strings_.Length; i++)
-    //    {
-    //        commonString_ = Find_Common_String(strings_[i], string_);
-    //        if (commonString_.Length > 0 && commonString_ != string_)
-    //            output.Add(commonString_);
-    //    }
-    //
-    //    return output;
-    //}
+    /*private List<string> Find_Common_Strings(string[] strings_, string string_)
+      {
+          List<string> output = new List<string>();
+          string commonString_;
+
+          for (int i = 0; i < strings_.Length; i++)
+          {
+              commonString_ = Find_Common_String(strings_[i], string_);
+              if (commonString_.Length > 0 && commonString_ != string_)
+                  output.Add(commonString_);
+          }
+
+          return output;
+      } */
 }
