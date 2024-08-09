2b26f3f93d575d743038918d669a14992eb9916b
yoayo112 <yoayo112@gmail.com>
2024-08-08 23:42:56 -0600

    working cannon mockup

--- Assets/Scripts/Combat/Minigames/2048/TFE_Launcher.cs
+++ Assets/Scripts/Combat/Minigames/2048/TFE_Launcher.cs
@@ -12,14 +12,13 @@ public class TFE_Launcher : MonoBehaviour
     private float depth; // the distance between the canon and the wall
     private float height; // the height of the WALL, not the arc.
     private Vector3 origin; // the location of the canon
-    private float velocity = 17.98f;
+    public float velocity = 17.98f;
     public float mass = 5;
     // Start is called before the first frame update
     void Start()
     {
         origin = spawnPoint.transform.position;
         depth = wall.transform.position.z - origin.z;
-        Debug.Log("wall:" + wall.transform.position.z + " - origin " + origin.z + " = depth " + depth);
         height = 10;
     }
 
@@ -36,32 +35,44 @@ public class TFE_Launcher : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             float y = target.y - origin.y;
-            Debug.Log("target.y " + target.y + " - origin.y " + origin.y + " = " + (target.y - origin.y));
+            float x = target.x - origin.x;
             //create a cube with mass and physics at the given spawn point.
-            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            cube.transform.position = spawnPoint.transform.position;
-            Rigidbody rb = cube.AddComponent<Rigidbody>();
+            GameObject projectile = Instantiate(Resources.Load<GameObject>("2048Bubble"));
+            //GameObject projectile = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            projectile.transform.position = spawnPoint.transform.position;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
             rb.mass = mass;
-            float v = velocity + ((4.45f * y) / height);
+            
 
             //ok time to math this out. Hello trig.
 
             //the relationship between target.y (or just y) and the total length of the arc is
             //R = d + ((d * y)/h
             float r = depth + ((depth*y)/height);
-            Debug.Log("d + (dy/h) = "+r);
 
-            //theta = arcsin(rg/v^2)
+            //This apporximation works!
+            // - theta = arcsin(rg/v^2)
+            float v = velocity + ((4.45f * y) / height);
             float theta = Mathf.Asin((r * 9.81f) / (v * v))/2;
-            Debug.Log("th = (asin((r*9.81)/(v^2))/2) = " + theta);
-            //theta = theta * Mathf.Rad2Deg;
-            Debug.Log("as radians: " + theta);
+
+            //Lets try seansters monster equation - :(
+            //float v = velocity;
+            //float k = (2 * 9.81f * y) / (v * v);
+            //float bigNumerator = (0 - 0.125f) * Mathf.Sqrt((16 * (y * y)) + (8 * y) + 1) * ((0 - ((64 * (y - 1)) / (((4 * y) + 1) * ((4 * y) + 1)))) + (64 / ((4 * y) + 1)) - (512 / (((4 * y) + 1) * ((4 * y) + 1))));
+            //float bigTerm = 0.5f * Mathf.Sqrt((bigNumerator / Mathf.Sqrt(3 - (4 * y))) - (((8 * y) - 2) / ((4 * y) + 1)) + ((2 * ((4 * y) - 1)) / ((4 * y) + 1)) + (32 / (((4 * y) + 1) * ((4 * y) + 1))));
+            //float atan = (Mathf.Sqrt(3 - (4 * y)) / Mathf.Sqrt((16 * (y * y)) + (8 * y) + 1)) + bigTerm + (2 / (4 * y) + 1);
+            //float theta = 2 * Mathf.PI - (2 * Mathf.Atan(atan));
+
+            // - phi = tan(distance x/depth)
+            float phi = Mathf.Tan(x / (1.5f*depth));
+
 
             float fz = Mathf.Cos(theta) * v;
             float fy = Mathf.Sin(theta) * v;
-            Debug.Log("fy= " + fy + " ,fz= " + fz);
 
-            Vector3 force = new Vector3(0,fy, fz);
+            float fx = Mathf.Sin(phi) * v;
+
+            Vector3 force = new Vector3(fx, fy, fz);
             rb.AddForce(force, ForceMode.Impulse);
 
         }
