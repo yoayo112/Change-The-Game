172f05fc6a90f00a6c5179c2edf3c41cb6f55e76
yoayo112 <yoayo112@gmail.com>
2024-08-15 00:28:36 -0600

    implemented animation. a touch chopy, but ready for playtest before more investment

---
+++ Assets/Scripts/Combat/Minigames/2048/TFE_MergeAnimation.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TFE_MergeAnimation : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Combat/Minigames/2048/TFE_Merger.cs
+++ Assets/Scripts/Combat/Minigames/2048/TFE_Merger.cs
@@ -11,6 +11,7 @@ handles merges between collisions in the 2048 minigame. Attached to the gravity 
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class TFE_Merger : MonoBehaviour
@@ -96,16 +97,14 @@ public class TFE_Merger : MonoBehaviour
             }
             if(bubbleResource != null)
             {
-                GameObject newBubble = Instantiate(bubbleResource);
-                newBubble.transform.position = request.position;
+                //Cover both objects with animation
+                StartCoroutine(animateMerge(request.objects, bubbleResource));
+
 
                 //remove pending request
                 mergeRequests.Remove(request.objects[0]);
                 pendingMerges.Remove(request);
 
-                //delete both objects
-                Destroy(request.objects[0]);
-                Destroy(request.objects[1]);
             }
             else //if they cant be mergered, just remove the request
             {
@@ -114,5 +113,82 @@ public class TFE_Merger : MonoBehaviour
                 pendingMerges.Remove(request);
             }
         }
+    }
+
+    private IEnumerator animateMerge(GameObject[] spheres, GameObject bubbleResource)
+    {
+        //get both spawn points.
+        Vector3 P1 = spheres[0].transform.position;
+        Vector3 P3 = spheres[1].transform.position;
+
+        //spawn and position animation prefab
+        GameObject mergeAnimate = Instantiate(Resources.Load<GameObject>("MergeAnimation"));
+        mergeAnimate.transform.position = P1;
+        GameObject pointA = mergeAnimate.GetComponentInChildren<TFE_MergeAnimation>().pointA.gameObject;
+        GameObject pointB = mergeAnimate.GetComponentInChildren<TFE_MergeAnimation>().pointB.gameObject;
+
+        //scale prefab to correct size
+        mergeAnimate.transform.localScale = new Vector3(
+            spheres[0].transform.localScale.x * mergeAnimate.transform.localScale.x, 
+            spheres[0].transform.localScale.y * mergeAnimate.transform.localScale.y,
+            spheres[0].transform.localScale.z * mergeAnimate.transform.localScale.z);
+
+        //change prefab materiel
+        pointA.GetComponent<SkinnedMeshRenderer>().material = spheres[0].gameObject.GetComponent<MeshRenderer>().materials[0];
+        pointB.GetComponent<SkinnedMeshRenderer>().material = spheres[0].gameObject.GetComponent<MeshRenderer>().materials[0];
+
+        Vector3 P2 = pointB.transform.position;
+        //For this trig we are using 3 points, P1, P2, P3 in 3d space, (x,y,z)
+        //I am going to define some coordinate differences below, since each will be used 1-2x when finding segment lengths
+        float X1 = Mathf.Abs(P3.x - P1.x);     float Y1 = Mathf.Abs(P3.y - P1.y);     float Z1 = Mathf.Abs(P3.z - P1.z);
+        float X2 = Mathf.Abs(P2.x - P1.x);     float Y2 = Mathf.Abs(P2.y - P1.y);     float Z2 = Mathf.Abs(P2.z - P1.z);
+        float X3 = Mathf.Abs(P3.x - P2.x);     float Y3 = Mathf.Abs(P3.y - P2.y);     float Z3 = Mathf.Abs(P3.z - P2.z);
+
+        //Now I can define some segment lengths as:
+        float A = Mathf.Sqrt((Y1 * Y1) + (Z1 * Z1));
+        float B = Mathf.Sqrt((Y2 * Y2) + (Z2 * Z2));
+        float C = Mathf.Sqrt((Y3 * Y3) + (Z3 * Z3));
+
+        float D = Mathf.Sqrt((X1 * X1) + (Z1 * Z1));
+        float E = Mathf.Sqrt((X2 * X2) + (Z2 * Z2));
+        float F = Mathf.Sqrt((X3 * X3) + (Z3 * Z3));
+        //finally, I can find the angle I need to rotate the shape over the z axis as:
+        float theta = Mathf.Acos(((A * A) + (B * B) - (C * C)) / (2 * A * B));
+        theta *= Mathf.Rad2Deg;
+        //and the angle I need to rotate the sape over the y axis as:
+        float phi = Mathf.Acos(((D*D)+(E*E)-(F*F))/(2*D*E));
+        phi *= Mathf.Rad2Deg;
+
+        //adjust prefab to cover both spheres
+        //Vector3 targetLine = P1 - P2; //find the vector representing the distance between the original objects
+        //now we need to rotate the whole object until the line between the animated sphered == this target line
+        mergeAnimate.transform.Rotate(new Vector3(0, phi, theta));
+
+        //Ok lastly, it,s not spawning the new object in the correct place. So I am going to find the center of the animation object.
+        //i.e. spawn new bubble halfway between sphere a and sphere b
+        Vector3 centerLength = P1 - P3;
+        float centerLength_x = centerLength.x / 2;
+        float centerLength_y = centerLength.y / 2;
+        float centerLength_z = centerLength.z / 2;
+        Vector3 center = new Vector3(
+            P1.x + centerLength_x,
+            P1.y,
+            P1.z + centerLength_z);
+
+
+        //delete both objects
+        Destroy(spheres[0]);
+        Destroy(spheres[1]);
+
+        //start->wait for animation to finish
+        yield return GlobalService.AnimWait(mergeAnimate.GetComponentInChildren<Animator>(), "Merge", "Seperate");
+
+        //add resulting bubble
+        GameObject newBubble = Instantiate(bubbleResource);
+        newBubble.transform.position = center;
+
+        //delete animation
+        Destroy(mergeAnimate);
+        yield return null;
     }
 }
